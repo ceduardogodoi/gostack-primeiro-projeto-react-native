54ad8a53903d09a3005dde1af07db3609d0da8eb
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-12-14 12:22:08 -0300

    Arquivos faltantes do commit React Navigation

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
 
 import './config/ReactotronConfig';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
-
-console.tron.log('Hello World');
+import Routes from './routes';
 
 function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-    </View>
-  );
+  return <Routes />;
 }
 
 export default App;
