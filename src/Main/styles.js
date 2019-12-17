d41cdfe7b06ea82f117000ee9fb17d29480cfd42
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2019-12-16 22:34:49 -0300

    Acessando API do Github

--- src/Main/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components/native';
-
-const Container = styled.View`
-  flex: 1;
-  padding: 30px;
-`;
-
-export { Container };
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,8 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-import Main from './Main';
-import User from './User';
+import Main from './pages/Main';
+import User from './pages/User';
 
 const Routes = createAppContainer(
   createStackNavigator(
